374fb49960a4007a6d0b5a5ff11405b9b8fa8454
Chuy Medina <chuymedina96@gmail.com>
2017-09-27 14:22:05 -0500

    ready for deployment

---
+++ app.js
@@ -0,0 +1,16 @@
+var express 	= require("express"),
+	app			= express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+app.get("/home", function(req, res){
+	res.render("home");
+});
+app.listen(3000, function(){
+	console.log("Server has started!");
+});
